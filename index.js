e5cc03cc03fc167ef620cbe9d82eb3b70bec9847
Nehan Yaser <67751528+webdevsint@users.noreply.github.com>
2022-10-07 13:03:21 +0600

    admin authentication added

--- index.js
+++ index.js
@@ -1,7 +1,10 @@
+const session = require("express-session");
+const passport = require("passport");
 const express = require("express");
 const axios = require("axios");
 const cors = require("cors");
 require("dotenv").config();
+require("./auth");
 
 const app = express();
 
@@ -11,11 +14,59 @@ const KEY = process.env.API_KEY;
 
 app.use(cors());
 
+app.use(session({ secret: "cats", resave: false, saveUninitialized: true }));
+app.use(passport.initialize());
+app.use(passport.session());
+
 app.set("view engine", "ejs");
-app.use(express.static('public'))
+app.use(express.static("public"));
+
+function isLoggedIn(req, res, next) {
+  if (req.user) {
+    if (req.user.email === "nehanyaser@gmail.com") {
+      next();
+    } else if (req.user.email !== "nehanyaser@gmail.com") {
+      req.logout(() => {
+        req.session.destroy();
+        res.send("You are not an admin!");
+      });
+    }
+  } else {
+    res.redirect("/login");
+  }
+}
+
+app.get("/login", (req, res) => {
+  res.redirect("/auth/google");
+});
+
+app.get("/logout", (req, res) => {
+  req.logout(() => {
+    req.session.destroy();
+    res.send("Logged out...");
+  });
+});
+
+// authentication
+app.get(
+  "/auth/google",
+  passport.authenticate("google", { scope: ["email", "profile"] })
+);
+
+app.get(
+  "/google/callback",
+  passport.authenticate("google", {
+    successRedirect: "/admin/posts",
+    failureRedirect: "/auth/google/failure",
+  })
+);
+
+app.get("/auth/google/failure", (req, res) => {
+  res.send("Failed to authenticate..");
+});
 
 app.get("/", (req, res) => {
-  res.send("welcome to the deranged podcast api. are you an admin ?");
+  res.send("welcome to the deranged blog api. are you an admin ?");
 });
 
 // returns a specific post
@@ -41,7 +92,7 @@ app.get("/post/:id", (req, res) => {
 });
 
 // returns all posts
-app.get("/admin/posts", (req, res) => {
+app.get("/admin/posts", isLoggedIn, (req, res) => {
   axios
     .get(API, {
       params: { key: KEY },
@@ -55,7 +106,7 @@ app.get("/admin/posts", (req, res) => {
 });
 
 // form to create a new post
-app.get("/admin/new", (req, res) => {
+app.get("/admin/new", isLoggedIn, (req, res) => {
   res.render("new", { api: API, key: KEY });
 });
 
